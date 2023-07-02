2bce5a1ece27128b74bbd2e1254a23cda9904651
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-07-02 13:07:10 -0600

    Minor formatting changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,20 +22,19 @@ export default function Home() {
       <div>
         <h1>Unstuck!</h1>
         <h3>
-          This is a platform for students to get experience through real-world projects to gain a
-          greater understanding of their skills, talents, interests and personality traits
+          This is a platform for students to get experience through real-world projects to gain a greater understanding of their
+          skills, talents, interests and personality traits
         </h3>
         <p>
-          Sherpa's lead students through developing problem statements called "Stucks" which refer
-          to the topic that the sherpa has laid out. Then students start out on an Expedition,
-          uncovering deeper learning and knowledge about the Stuck they are diving into. Students
-          are trained in the process of using critical thinking and design thinking to get more
+          Sherpa's lead students through developing problem statements called "Stucks" which refer to the topic that the sherpa
+          has laid out. Then students start out on an Expedition, uncovering deeper learning and knowledge about the Stuck they
+          are diving into. Students are trained in the process of using critical thinking and design thinking to get more
           understanding of a problem than they've ever had before.
         </p>
         <p>
-          Unstuck helps students find direction during the project phase, guiding them through
-          self-reflection to get the most learning possible from completing their projects. This
-          will help give students direction after high-school, and it's all about self-exploration!
+          Unstuck helps students find direction during the project phase, guiding them through self-reflection to get the most
+          learning possible from completing their projects. This will help give students direction after high-school, and it's all
+          about self-exploration!
         </p>
       </div>
       <div
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,63 +1,61 @@
-
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { supabase } from '../supabaseClient';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { supabase } from "../supabaseClient";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 function Copyright(props) {
   return (
-    <Typography variant="body2" color="text.secondary" align="center" {...props}>
-      {'Copyright © '}
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
       <Link color="inherit" href="/">
         Unstuck
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
 
-
 const defaultTheme = createTheme();
 
-
-
 export default function SignIn() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
-  const navigate = useNavigate()
-  
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      const formData = new FormData(event.currentTarget);
-      setLoading(true)
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email: formData.get('email'),
-        password: formData.get('password'),
-      },)
-  
-      if (error) {
-        alert(error.error_description || error.message)
-      } else {
-          navigate("/student-dashboard",)
-        
-      }
-      setLoading(false)
-    };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    setLoading(true);
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: formData.get("email"),
+      password: formData.get("password"),
+    });
 
+    if (error) {
+      alert(error.error_description || error.message);
+    } else {
+      navigate("/student-dashboard");
+    }
+    setLoading(false);
+  };
 
   return (
     <ThemeProvider theme={defaultTheme}>
@@ -66,18 +64,23 @@ export default function SignIn() {
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
             <TextField
               margin="normal"
               required
