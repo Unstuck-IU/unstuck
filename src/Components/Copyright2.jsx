4a07b3e6542389e668a5f8eab5acd3cee74ff10d
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-07-05 12:20:12 -0600

    Created re-usable component for copyright text

---
+++ src/Components/Copyright2.jsx
@@ -0,0 +1,22 @@
+import Link from "@mui/material/Link";
+import Typography from "@mui/material/Typography";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}>
+      {"Copyright © "}
+      <Link
+        color="inherit"
+        href="/">
+        Unstuck
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+export default Copyright;
