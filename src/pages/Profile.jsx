41d8cf87fd0d288e8c224b466d25cd9315c1aa51
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-07-07 21:57:50 -0600

    Deleted unnecessary code

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -33,16 +33,16 @@ const Profile = () => {
   // auth.userLocal needs to give us more details than just user_id, so we can update page details like name, avatar, etc.
 
   // ui elements
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [userType, setUserType] = useState("student");
-  const [displayName, setDisplayName] = useState("");
-  const [completedSignup, setCompletedSignup] = useState(true);
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [userType, setUserType] = useState("student");
+  // const [displayName, setDisplayName] = useState("");
+  // const [completedSignup, setCompletedSignup] = useState(true);
   const [fetchError, setFetchError] = useState("");
   const [userDetails, setUserDetails] = useState(null);
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
   const auth = useAuth();
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
@@ -84,232 +84,6 @@ const Profile = () => {
   }, []);
 
   return (
-    // <Box m="50px">
-    //   {/* HEADER */}
-    //   <Box
-    //     display="flex"
-    //     justifyContent="space-between"
-    //     alignItems="center">
-    //     <Header
-    //       title="Profile"
-    //       subtitle="Welcome to your Unstuck Profile"
-    //     />
-
-    //   </Box>
-    //   <Box
-    //     display="flex"
-    //     justifyContent="center"
-    //     alignItems="center">
-    //     <UpdateProfileForm />
-    //   </Box>
-
-    //   {/* GRID & CHARTS */}
-    //   <Box
-    //     display="grid"
-    //     gridTemplateColumns="repeat(12, 1fr)"
-    //     grid-template-rows="repeat(3, 1fr)"
-    //     grid-row-gap="2rem" /* Will be used instead by browsers that do not support `row-gap` */
-    //     row-gap="2rem"
-    //     // gridAutoRows="200px"
-    //     // gap="20px"
-    //   >
-    //     {/* <Paper> */}
-    //     {userDetails && (
-    //       <Box
-    //         gridColumn="span 12"
-    //         backgroundColor={colors.primary[400]}
-    //         display="flex"
-    //         alignItems="center"
-    //         justifyContent="center">
-    //         <Container>
-    //           <Typography variant="h2">
-    //             {userDetails.first_name} {userDetails.last_name}
-    //           </Typography>
-    //           {/* <Typography variant="p">This is the topic for the current class, which you will use to base you Stuck on.</Typography> */}
-    //           <Typography variant="h3">
-    //             <div className="user-details">{userDetails.display_name}</div>
-    //           </Typography>
-    //           <Typography variant="h2">Avatar
-    //           </Typography>
-    //         </Container>
-    //       </Box>
-    //     )}
-    //     {/* </Paper> */}
-    //   </Box>
-    //   <Box
-    //     gridColumn="span 3"
-    //     backgroundColor={colors.primary[400]}
-    //     display="flex"
-    //     alignItems="center"
-    //     justifyContent="center">
-    //     <Container>
-    //       <Typography variant="h2">Badges and achievements can go here</Typography>
-    //     </Container>
-    //   </Box>
-    //   <Box
-    //     gridColumn="span 3"
-    //     backgroundColor={colors.primary[400]}
-    //     display="flex"
-    //     alignItems="center"
-    //     justifyContent="center">
-    //     <Container>
-    //       <Typography variant="h2">My Stucks and Unstucks</Typography>
-    //     </Container>
-    //   </Box>
-    //   {/* <Box
-    //     gridColumn="span 3"
-    //     backgroundColor={colors.primary[400]}
-    //     display="flex"
-    //     alignItems="center"
-    //     justifyContent="center">
-    //     <StatBox
-    //       title="1,325,134"
-    //       subtitle="Traffic Received"
-    //       progress="0.80"
-    //       increase="+43%"
-    //       icon={<TrafficIcon sx={{ color: colors.greenAccent[600], fontSize: "26px" }} />}
-    //     />
-    //   </Box> */}
-    //   {/*
-    //   <Box
-    //     gridColumn="span 8"
-    //     gridRow="span 2"
-    //     backgroundColor={colors.primary[400]}>
-    //     <Box
-    //       mt="25px"
-    //       p="0 30px"
-    //       display="flex "
-    //       justifyContent="space-between"
-    //       alignItems="center">
-    //       <Box>
-    //         <Typography
-    //           variant="h5"
-    //           fontWeight="600"
-    //           color={colors.grey[100]}>
-    //           Revenue Generated
-    //         </Typography>
-    //         <Typography
-    //           variant="h3"
-    //           fontWeight="bold"
-    //           color={colors.greenAccent[500]}>
-    //           $59,342.32
-    //         </Typography>
-    //       </Box>
-    //       <Box>
-    //         <IconButton>
-    //           <DownloadOutlinedIcon sx={{ fontSize: "26px", color: colors.greenAccent[500] }} />
-    //         </IconButton>
-    //       </Box>
-    //     </Box>
-    //     <Box
-    //       height="250px"
-    //       m="-20px 0 0 0">
-    //       <LineChart isDashboard={true} />
-    //     </Box>
-    //   </Box>
-    //   <Box
-    //     gridColumn="span 4"
-    //     gridRow="span 2"
-    //     backgroundColor={colors.primary[400]}
-    //     overflow="auto">
-    //     <Box
-    //       display="flex"
-    //       justifyContent="space-between"
-    //       alignItems="center"
-    //       borderBottom={`4px solid ${colors.primary[500]}`}
-    //       colors={colors.grey[100]}
-    //       p="15px">
-    //       <Typography
-    //         color={colors.grey[100]}
-    //         variant="h5"
-    //         fontWeight="600">
-    //         Recent Transactions
-    //       </Typography>
-    //     </Box>
-    //     {mockTransactions.map((transaction, i) => (
-    //       <Box
-    //         key={`${transaction.txId}-${i}`}
-    //         display="flex"
-    //         justifyContent="space-between"
-    //         alignItems="center"
-    //         borderBottom={`4px solid ${colors.primary[500]}`}
-    //         p="15px">
-    //         <Box>
-    //           <Typography
-    //             color={colors.greenAccent[500]}
-    //             variant="h5"
-    //             fontWeight="600">
-    //             {transaction.txId}
-    //           </Typography>
-    //           <Typography color={colors.grey[100]}>{transaction.user}</Typography>
-    //         </Box>
-    //         <Box color={colors.grey[100]}>{transaction.date}</Box>
-    //         <Box
-    //           backgroundColor={colors.greenAccent[500]}
-    //           p="5px 10px"
-    //           borderRadius="4px">
-    //           ${transaction.cost}
-    //         </Box>
-    //       </Box>
-    //     ))}
-    //   </Box>
-
-    //   <Box
-    //     gridColumn="span 4"
-    //     gridRow="span 2"
-    //     backgroundColor={colors.primary[400]}
-    //     p="30px">
-    //     <Typography
-    //       variant="h5"
-    //       fontWeight="600">
-    //       Campaign
-    //     </Typography>
-    //     <Box
-    //       display="flex"
-    //       flexDirection="column"
-    //       alignItems="center"
-    //       mt="25px">
-    //       <ProgressCircle size="125" />
-    //       <Typography
-    //         variant="h5"
-    //         color={colors.greenAccent[500]}
-    //         sx={{ mt: "15px" }}>
-    //         $48,352 revenue generated
-    //       </Typography>
-    //       <Typography>Includes extra misc expenditures and costs</Typography>
-    //     </Box>
-    //   </Box>
-    //   <Box
-    //     gridColumn="span 4"
-    //     gridRow="span 2"
-    //     backgroundColor={colors.primary[400]}>
-    //     <Typography
-    //       variant="h5"
-    //       fontWeight="600"
-    //       sx={{ padding: "30px 30px 0 30px" }}>
-    //       Sales Quantity
-    //     </Typography>
-    //     <Box
-    //       height="250px"
-    //       mt="-20px">
-    //       <BarChart isDashboard={true} />
-    //     </Box>
-    //   </Box>
-    //   <Box
-    //     gridColumn="span 4"
-    //     gridRow="span 2"
-    //     backgroundColor={colors.primary[400]}
-    //     padding="30px">
-    //     <Typography
-    //       variant="h5"
-    //       fontWeight="600"
-    //       sx={{ marginBottom: "15px" }}>
-    //       Geography Based Traffic
-    //     </Typography>
-    //   </Box>
-    // </Box> */}
-
-    // </Box>
     <Box
       gridColumn="span 12"
       justifyContent="space-between"
@@ -330,13 +104,13 @@ const Profile = () => {
               md={8}>
               <Item>
                 <Container>
+                  <AccountCircleIcon />
                   <Typography variant="h4">
-                    {userDetails.first_name} {userDetails.last_name}
-                  </Typography>
-                  <Typography variant="h5">
                     <div className="user-details">{userDetails.display_name}</div>
                   </Typography>
-                  <Typography variant="h5">Avatar</Typography>
+                  <Typography variant="h5">
+                    {userDetails.first_name} {userDetails.last_name}
+                  </Typography>
                 </Container>
               </Item>
             </Grid>
@@ -361,12 +135,12 @@ const Profile = () => {
             md={8}>
             <Item>
               <Container>
-                <Typography variant="h4">Stucks and Unstucks</Typography>
+                <Typography variant="h4">Placeholder</Typography>
                 <ul>
                   Placeholder
-                  <li>Stuck 1</li>
-                  <li>Stuck 2</li>
-                  <li>Stuck 3</li>
+                  <li>Placeholder 1</li>
+                  <li>Placeholder 2</li>
+                  <li>Placeholder 3</li>
                 </ul>
               </Container>
             </Item>
