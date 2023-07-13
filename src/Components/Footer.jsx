32ad481eae615d62d8ac358559b445d7807c0cd5
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-07-13 13:35:31 -0600

    Removed extra footer tag

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,6 +9,7 @@ function Footer() {
   return (
     <footer>
       <Copyright2 />
+      {/* <a href="https://www.freepik.com/free-vector/cartoon-nature-landscape-with-mountain-forest-deciduous-trees-trunks-clearance_10385782.htm#page=7&query=cartoon%20climbing%20mountain&position=0&from_view=search&track=ais">Image by upklyak</a> on Freepik */}
     </footer>
   );
 }
