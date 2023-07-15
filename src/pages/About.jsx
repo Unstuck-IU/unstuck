47ce2c88c12423eed1aaceb3972f9ba2b7f022c0
Jennifer Gajdos-Bomben <jgajdosbomben@gmail.com>
2023-07-14 20:13:14 -0600

    Added image credit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import Box from "@mui/material/Box";
+import Link from "@mui/material/Link";
 
 const About = () => {
   return (
     <div>
-      <h1>About</h1>
+      <Box>
+        <h1>About</h1>
+        <h4>Image credit:</h4>
+        <a href="https://www.freepik.com/free-vector/cartoon-nature-landscape-with-mountain-forest-deciduous-trees-trunks-clearance_10385782.htm#page=7&query=cartoon%20climbing%20mountain&position=0&from_view=search&track=ais">
+          Image by upklyak
+        </a>{" "}
+        on Freepik
+      </Box>
     </div>
   );
 };
